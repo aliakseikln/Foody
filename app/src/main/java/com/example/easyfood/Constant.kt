6954734534d13955a4fd90fbf7bd31c62636c1af
TURBO BEE <pavidlo911@gmail.com>
2022-07-21 20:57:34 +0300

    First push of my Food app

--- app/src/main/java/com/example/easyfood/Constant.kt
+++ app/src/main/java/com/example/easyfood/Constant.kt
@@ -5,4 +5,5 @@ const val MEAL_NAME = "com.example.easyfood.fragments.nameMeal"
 const val MEAL_THUMB = "com.example.easyfood.fragments.thumbMeal"
 const val CATEGORY_NAME = "com.example.easyfood.fragments.categoryName"
 const val MEAL_AREA = "com.example.easyfood.fragments.mealArea"
+
 const val MEAL_STR = "com.example.easyfood.fragments.mealStr"
